436e69aa438b0aad4e0020b1889572b08953e8c6
Louis Lagrange <louis.lagrange@outlook.fr>
2021-06-01 12:20:11 +0200

    App fonctionnelle, bug sur l'affichage des messages ?

--- app/controllers/files_controller.rb
+++ app/controllers/files_controller.rb
@@ -4,53 +4,42 @@ class FilesController < ApplicationController
         require 'rubygems'
         require 'rest-client'
 
-        require 'net/http'
         url = "https://exomo.grafana.net/api/datasources"
-
-        #uri = URI(url)
-        #req = Net::HTTP::Get.new(uri)
-        #req['Authorization'] = "Bearer eyJrIjoiNDdETDVnZ3p0STJxaktyN1VZY2F4c3MyWmFtNTJrNVgiLCJuIjoiZXhvbW8iLCJpZCI6MX0="
-
-        #res = Net::HTTP.start(uri.hostname, uri.port) {|http|
-        #   http.request(req)
-        #}
-
-        #@body = res.body
 
         @response_get = RestClient::Request.execute(:method => 'get', :url => url, :headers => {:Authorization => 'Bearer eyJrIjoiNDdETDVnZ3p0STJxaktyN1VZY2F4c3MyWmFtNTJrNVgiLCJuIjoiZXhvbW8iLCJpZCI6MX0='})
         
-
-        response_post = RestClient::Request.new({
+        response = RestClient::Request.new({
             method: :post,
             url: url, 
             payload: {type: 'marcusolsson-csv-datasource',
-                    name:'rubyDS',
-                    url: 'https://dl.dropbox.com/s/mw3z35tlebe456a/0x341513238FCF_2021-01-27_14-26-11.csv?row=1', 
+                    name:'Marc_Coffinet_RoR',
+                    url: 'https://dl.dropbox.com/s/27ff5w2yuidt3fi/0x341513238FCF_2021-05-09_19-43-55.csv?row=1', 
                     access: 'proxy', 
                     basicAuth: false},
             headers: {Authorization:'Bearer eyJrIjoiNDdETDVnZ3p0STJxaktyN1VZY2F4c3MyWmFtNTJrNVgiLCJuIjoiZXhvbW8iLCJpZCI6MX0=',
                     accept: JSON,
                     content_type: :JSON}
-            }).execute do |response_post, request, result|
+            }).execute do |response, request, result|
 
-            case response_post.code
+            case response.code
             when 400
-                [ :error, JSON.parse(response_post.to_str) ]
+                [ :error, JSON.parse(response.to_str) ]
                 @return_request = "error = 400"
             when 200
-                [ :success, JSON.parse(response_post.to_str) ]
+                [ :success, JSON.parse(response.to_str) ]
                 @return_request = "success = 200"
             else
-                ["unknow error, find it !", response_post.code]
+                ["unknow error, find it !", response.code]
             end
         end
 
-        @response = response_post
-        #$auth = 'Bearer eyJrIjoiNDdETDVnZ3p0STJxaktyN1VZY2F4c3MyWmFtNTJrNVgiLCJuIjoiZXhvbW8iLCJpZCI6MX0='
-        
-        
-        #@resource = RestClient::Resource.new( $url )
-        #@response = @resource.get( :Authorization => $auth )
+        @response_post = response
+
+        # Utilisation de la gem RestClient pour une requete "get" (demande d'information)
+
+            #$auth = 'Bearer eyJrIjoiNDdETDVnZ3p0STJxaktyN1VZY2F4c3MyWmFtNTJrNVgiLCJuIjoiZXhvbW8iLCJpZCI6MX0='
+            #@resource = RestClient::Resource.new( $url )
+            #@response = @resource.get( :Authorization => $auth )
     
     end
 
